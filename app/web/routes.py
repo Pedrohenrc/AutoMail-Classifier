2f4c38d275a88b94161134cf8ec1f9ace1213e25
Pedrohenrc <pirriquecsousa@gmail.com>
2026-01-16 17:48:02 -0300

    Trocando o mock por IA

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, Form, HTTPException
 from fastapi.responses import HTMLResponse
-
+from app.infrastructure.exceptions.ai_unavaliable_exception import AIUnavailableException
 from app.use_cases.analyze_email_use_case import AnalyzeEmailUseCase
-from app.infrastructure.ai.mock_classifier import MockClassifier
-from app.infrastructure.ai.mock_response import MockResponder
+from app.infrastructure.ai.openai_classifier import OpenAIClassifier
+from app.infrastructure.ai.openai_response import OpenAIResponse
 
 router = APIRouter()
 
@@ -26,21 +26,25 @@ def index():
 @router.post("/", response_class=HTMLResponse)
 def analyze_email(email_text: str = Form(...)):
     use_case = AnalyzeEmailUseCase(
-        classifier=MockClassifier(),
-        responder=MockResponder()
+        classifier=OpenAIClassifier(),
+        responder=OpenAIResponse()
     )
-
-    result = use_case.execute(email_text)
-
-    return f"""
-    <html>
-        <body>
-            <h2>Resultado</h2>
-            <p><strong>Classificação:</strong> {result.classification.value}</p>
-            <p><strong>Resposta sugerida:</strong></p>
-            <pre>{result.suggested_response}</pre>
-            <br>
-            <a href="/">Voltar</a>
-        </body>
-    </html>
-    """
+    try:
+        result = use_case.execute(email_text)
+        return f"""
+        <html>
+            <body>
+                <h2>Resultado</h2>
+                <p><strong>Classificação:</strong> {result.classification.value}</p>
+                <p><strong>Resposta sugerida:</strong></p>
+                <pre>{result.suggested_response}</pre>
+                <br>
+                <a href="/">Voltar</a>
+            </body>
+        </html>
+        """
+    except AIUnavailableException:
+        raise HTTPException(
+            status_code=503,
+            detail="Serviço de IA temporariamente indisponível"
+        )
